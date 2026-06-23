257360fba1ed179a84466a70fc2cbab694b85862
Steven <steven25@itbss.ac.id>
2026-06-23 10:24:21 +0700

    fix css login

--- login.php
+++ login.php
@@ -3,30 +3,39 @@ include "koneksi.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="shortcut icon" href="img/anekagalery_32x32.png">
-        <title>Login - Aneka Gallery</title>
-        <link rel="stylesheet" href="css/login.css">
-    </head>
-    <body>
-        <section class="login">
-            <h2>Login</h2>
-            <form action="sv_login.php" method="post" name="form">
-                <div class="login_form">
-                    <label for="username">Username</label>
-                    <input type="text" name="username" placeholder="username">
-                </div>
-                <div class="login_form">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" placeholder="password">
-                </div>
-                <button type="submit">Login</button>
-                <p>Dont have a account?
-                    <a href="pendaftaran_form.php">Create Account</a>
-                </p>
-            </form>
-        </section>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/anekagalery_32x32.png">
+    <title>Login - Aneka Galery</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <section class="login">
+
+    <div class="login_left"></div>
+
+    <div class="login_right">
+        <h2>Login</h2>
+        <form action="sv_login.php" method="post">
+            <div class="login_form">
+                <label>Username</label>
+                <input type="text" name="username" required>
+            </div>
+
+            <div class="login_form">
+                <label>Password</label>
+                <input type="password" name="password" required>
+            </div>
+
+            <button type="submit">Sign In</button>
+            <p>
+                Don't have an account?
+                <a href="pendaftaran_form.php">Create Account</a>
+            </p>
+        </form>
+    </div>
+
+</section>
+</body>
 </html>
