17709e8efe0819e30cdab5c74337cea0ff80e4ec
Larparc . <defrio.andersen55@gmail.com>
2026-07-10 20:27:27 +0700

    project  admin

--- admin/project.php
+++ admin/project.php
@@ -74,29 +74,6 @@ include "security.php";
 
 <div class="content">
 
-<?php
-include "koneksi.php";
-$qry = $koneksi->query("SELECT * FROM user");
-?>
-
-<div class="container">
-    <a href="tambahData.php" class="add">Tambah Data</a>
-    <div class="content">
-        <?php if($qry->num_rows > 0){
-            while($data = $qry->fetch_assoc()){?>
-                <div class="card">
-                    <img src="assets/images/<?= $data['profile'];?>" alt="">
-                    <div class="card-body">
-                        <p class="title"><?= $data['name'];?></p>
-                        <a href="detail.php?id=<?= $data['id_user'];?>" class="detail">Detail</a>
-                        <a href="delete.php?id=<?= $data['id_user'];?>" class="detail">Hapus</a>
-                    </div>
-                </div>
-            <?php }?>
-        <?php }else{ 
-            echo "Tidak Ada Data";
-        }?>
-    </div>
 </div>
 </div>
 </div>
